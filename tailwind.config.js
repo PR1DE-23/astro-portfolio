ec3a35a52fdc7b86f8899a631d8951e84640532a
Daniel <pr1de_23@proton.me>
2026-02-18 17:15:26 -0500

    Cambio de fuente de iconos

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,23 @@ export default {
         'display': ['var(--font-display)', 'sans-serif'],
       },
       colors: {
-        accent: 'var(--accent)',
-        secondary: 'var(--secondary)',
+        brand: {
+          DEFAULT: '#000000',
+          white: '#ffffff',
+          neutral: '#404040'
+        },
+        zinc: {
+          DEFAULT: '#000000',
+          200: '#e4e4e7',
+          300: '#d4d4d8',
+          500: '#71717a',
+          800: '#27272a',
+        },
+        error: {
+          DEFAULT: '#ef4444',
+        }
       }
-    },
+    },  
   },
   plugins: [],
 }
