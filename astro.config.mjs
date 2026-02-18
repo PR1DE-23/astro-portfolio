487dec201426b68a72c9c870c185062e33430cb3
Daniel <pr1de_23@proton.me>
2026-02-18 11:50:49 -0500

    cambios generales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  // ...
   integrations: [tailwind()],
-  // ...
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
 });
